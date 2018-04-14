9de7465c63e21496a482fc586ab166d67acb66a3
josuedavidgodinez <godinezjosue@hotmail.com>
2018-04-14 10:54:33 -0600

    ba

---
+++ EDL4/EDL4/DBContest/DefaultConnection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EDL4.Models;
+namespace EDL4.DBContest
+{
+    public class DefaultConnection
+    {
+        private static volatile DefaultConnection Instance;
+        private static object syncRoot = new Object();
+
+
+        public Dictionary<string, Pais> DiccionarioListados = new Dictionary<string, Pais>();
+
+
+        public Dictionary<string, Estampa> estampasdirectas = new Dictionary<string, Estampa>();
+
+        public List<Estampa> estampas = new List<Estampa>();
+
+
+        public int IDActual { get; set; }
+
+        private DefaultConnection()
+        {
+            IDActual = 0;
+        }
+
+        public static DefaultConnection getInstance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (Instance == null)
+                        {
+                            Instance = new DefaultConnection();
+                        }
+                    }
+                }
+                return Instance;
+            }
+        }
+    }
+}
--- EDL4/EDL4/Models/Pais.cs
+++ EDL4/EDL4/Models/Pais.cs
@@ -7,5 +7,10 @@ namespace EDL4.Models
 {
     public class Pais
     {
+
+        public string nombre { get; set; }
+        public List<int> faltantes = new List<int>();
+        public List<int> coleccionadas = new List<int>();
+        public List<int> cambios = new List<int>();
     }
 }
